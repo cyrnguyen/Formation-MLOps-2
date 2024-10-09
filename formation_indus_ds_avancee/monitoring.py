5b506717377b85a39f6b7a0dc41af8174f2fbf88
Cyril Nguyen <cyril.nguyen@datanumia.com>
2024-10-09 13:29:44 +0000

    Add monitoring in DAG

--- formation_indus_ds_avancee/monitoring.py
+++ formation_indus_ds_avancee/monitoring.py
@@ -21,6 +21,10 @@ def monitor_with_io(predictions_folder: str, db_con_str: str, monitoring_table_n
 
 def monitor(latest_predictions: pd.DataFrame) -> pd.DataFrame:
     # Start filling function
-    monitoring_df = pd.DataFrame
+    monitoring_df = (latest_predictions
+        .groupby(['predictions_time'], as_index=False)[['predictions']]
+        .mean()
+        .rename(columns={'predictions': 'predictions_avg'})
+    )
     # End filling function
     return monitoring_df
